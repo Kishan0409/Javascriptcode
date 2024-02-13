d3f2cd60caab27790382c946f6d5e9b5f17f0a93
Kishan0409 <raikishan79@gmail.com>
2024-02-13 15:48:34 +0530

    more of DOM , creating  node

---
+++ 05_iterations/five.js
@@ -0,0 +1,25 @@
+// ARRAY 
+
+
+const myArr = [0,1,2,3,4,5]
+
+const heroes = ["superman" ,"batman" , "shaktiman"]
+
+// console.log(myArr[1]);
+
+//ARRAY METHOD
+
+//const values = myArr.push(11);         //  Add element in the last index array .
+//myArr.pop(8);                         //   Remove last element in the index array .
+ //myArr.unshift(9)                     //   Add first element in the index array .
+//  myArr.shift()                       //    Remove first element in the index array .
+
+// console.log(myArr);
+ 
+// console.log(myArr.includes(9));
+
+// console.log(myArr.indexOf(1));
+
+
+const newArr = myArr.splice(1,3)
+console.log(newArr);
---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+  // PRACTISE SET  AND MAP METHOD
+
+//   const mynums = [1,2,3,4,5,6,7,8,9,10];
+
+//   const values = mynums.map((val) => {
+//     return val + 10
+//   })
+
+//   console.log(values);
+
+  let score = "hitesh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+
+
